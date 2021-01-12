2b57507b8b3674f842b0986cb12978235d429e41
迷渡 <justjavac@gmail.com>
2021-01-12 10:51:01 +0800

    fix namespace
    Signed-off-by: 迷渡 <justjavac@gmail.com>

--- mod.ts
+++ mod.ts
@@ -7,3 +7,11 @@ $("h2.title").text("Hello Deno!");
 $("h2").addClass("deno");
 
 console.log($.html());
+
+export interface HtmlImage {
+  readonly src?: string;
+  readonly alt?: string;
+  readonly width?: number | string;
+  readonly height?: number | string;
+  readonly imageElem: cheerio.CheerioElement;
+}
